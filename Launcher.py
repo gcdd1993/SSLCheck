b32fab69aa59e7942ac5842dc3369618909fc931
Musk.Chen <Musk.Chen@fanruan.com>
2022-11-19 17:19:05 +0800

    打印日志

--- Launcher.py
+++ Launcher.py
@@ -3,9 +3,10 @@
 # Created by gcdd1993 on 2022/11/19
 # Launcher.py
 
+from yaml import load
+
 from SSLCheck import SSLCheck
 from notify.QywxNotifyImpl import QywxNotifyImpl
-from yaml import load, dump
 
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -13,7 +14,7 @@ except ImportError:
     from yaml import Loader, Dumper
 
 if __name__ == '__main__':
-    with open("config.sample.yaml", "r") as f:
+    with open("config.yaml", "r", encoding="UTF-8") as f:
         conf = load(f, Loader=Loader)
         print(f"读取配置文件 {conf}")
         # 创建SSLCheck实例
@@ -25,15 +26,13 @@ if __name__ == '__main__':
         for ssl_checker in ssl_checker_list:
             expire_in_days = ssl_checker.get_ssl_time()
             msg += f"域名 {ssl_checker.domain} 还有 {expire_in_days} 天到期\n"
+        print(msg)
         # 创建notify实例
         notify_list = []
         for n in conf["notify"]:
             if n["type"] == "QYWX":
-                mentioned_list = []
-                mentioned_mobile_list = []
-                if "mentioned" in n:
-                    mentioned_list = n["mentioned"]["name"]
-                    mentioned_mobile_list = n["mentioned"]["phone"]
+                mentioned_list = n.get("mentioned", []).get("name", [])
+                mentioned_mobile_list = n.get("mentioned", []).get("phone", [])
                 notify_list.append(QywxNotifyImpl(n["key"], mentioned_list, mentioned_mobile_list))
         for notify in notify_list:
             notify.notify(msg)
